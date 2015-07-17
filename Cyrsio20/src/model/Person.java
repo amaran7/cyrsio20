0a7c8fd4a0f8dd4fb95c96f4bd7ef027a79e72a6
Elsio Coelho <elsio.coelho@gmail.com>
2015-07-17 14:46:52 +0200

    adjeu

---
+++ Cyrsio20/src/model/Person.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.*;
+
+public class Person {
+	String lastName;
+	String firstName;
+	String phoneNumber;
+	String street;
+	String postalCode;
+	Date birthdate;
+}
